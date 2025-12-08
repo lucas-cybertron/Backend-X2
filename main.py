e8ec8115551cf45d4f1612588ce5416987da336f
coffeecliff <cauaneves976@gmail.com>
2025-12-08 15:02:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ ALLOWED_ORIGINS = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
